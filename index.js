3c3afa444138c3f39cc2da763eb9fe0602ac68df
Govind <govindup63@gmail.com>
2024-10-02 02:19:25 +0530

    inabled cors for localhost

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const { textRouter } = require('./routes/text');
 const app = express()
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
+
 
 
 app.use(express.json())
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use('/api/user', userRouter);
 app.use('/api/text', textRouter)
 
